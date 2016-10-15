a5dd10c3c172b7a4c3b6c52dd7de7a880f31e75d
Yongki <brave0681@gmail.com>
2016-10-16 02:08:30 +0900

    response 수정

--- routes/bot.js
+++ routes/bot.js
@@ -13,16 +13,36 @@ router.get('/keyboard', function(req, res, next) {
 
 router.post('/message', function(req, res, next) {
   if (req.body.content == "인사해"){
-    res.json({message : "안녕하세요", keyboard : keyboard})
+    res.json({
+      message : {
+        text : "안녕하세요"
+      },
+      keyboard : keyboard
+    });
   }
   else if(req.body.content == "사과해"){
-    res.json({message : "죄송합니다.", keyboard : keyboard})
+    res.json({
+      message : {
+        text : "죄송합니다"
+      },
+      keyboard : keyboard
+    });
   }
   else if(req.body.content == "꼬부기는"){
-    res.json({message : "꼬북꼬북.", keyboard : keyboard})
+    res.json({
+      message : {
+        text : "꼬북꼬북"
+      },
+      keyboard : keyboard
+    });
   }
   else {
-    res.json({message : "뭔소리야", keyboard : keyboard})
+    res.json({
+      message : {
+        text : "뭔소리야"
+      },
+      keyboard : keyboard
+    });
   }
 });
 
